38f3f4e195a9fddd22044bc1e1fbc82402f66169
Matthew Hartley <matthewh@ebi.ac.uk>
2023-12-19 12:54:50 +0000

    Fix typescript errors and remove hybrid rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  //output: 'hybrid',
+  integrations: [mdx(), preact()]
 });

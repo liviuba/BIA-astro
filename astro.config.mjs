c44ed36293903e620bad7c16526cf2ae1d3f6d60
Matthew Hartley <matthewh@ebi.ac.uk>
2023-12-19 13:07:46 +0000

    Remove preact import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 
-import preact from "@astrojs/preact";
+//import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
